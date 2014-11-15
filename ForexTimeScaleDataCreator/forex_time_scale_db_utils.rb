ade6b778d310e241d712f30d6c4283b4c8669a2e
mmmmmm888 <gs812q@gmail.com>
2014-11-15 01:44:49 +0000

    update

--- ForexTimeScaleDataCreator/forex_time_scale_db_utils.rb
+++ ForexTimeScaleDataCreator/forex_time_scale_db_utils.rb
@@ -11,13 +11,13 @@ class ForexTimeScaleDbUtils
             close REAL NOT NULL,
             high_minute_close_timestamp TIMESTAMP NOT NULL,
             low_minute_close_timestamp TIMESTAMP NOT NULL,
-            close_minute_close_timestamp TIMESTAMP NOT NULL
+            close_minute_close_timestamp TIMESTAMP NOT NULL UNIQUE
         );
         SQL
     end
     
     def self.create_table_name(currency_pair, time_scale)
-        currency_pair + "_" + time_scale.time_scale_string + "_" + time_scale.minute.to_s + "_" + "minute"
+        currency_pair + "_" + time_scale.minute.to_s + "_" + "minute"
     end
 end
         
