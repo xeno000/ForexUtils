02c41956f45f305ac569e47c923b2156dc88696c
mmmmmm888 <gs812q@gmail.com>
2014-11-15 06:57:32 +0000

    update

--- ForexTimeScaleDataCreator/main.rb
+++ ForexTimeScaleDataCreator/main.rb
@@ -26,8 +26,8 @@ result = Benchmark.realtime do
 end
 p "TimeRangeArrayFactory.create_array() #{result}s"
 
-db = ForexDb.new(currency_pair)
-time_scale_db = ForexTimeScaleDb.new(currency_pair, time_scale)
+@db = ForexDb.new(currency_pair)
+@time_scale_db = ForexTimeScaleDb.new(currency_pair, time_scale)
 
 chunk_size = 100000
 i = 0
@@ -52,17 +52,7 @@ time_range_array.each do |time_range|
     time_range_store.push(time_range)
     if chunk_size < time_range_store.count
         total += 1
-        forex_time_scale_data_record_array = Array.new
-        result = Benchmark.realtime do
-            forex_time_scale_data_record_array = db.select_forex_time_scale_data_record_array(time_range_store)
-        end
-        p "db.select_forex_time_scale_data_record_array() #{result}s"
-        
-        result = Benchmark.realtime do
-            time_scale_db.insert_forex_time_scale_data_record_array(forex_time_scale_data_record_array)
-        end
-        p "time_scale_db.insert_forex_time_scale_data_record_array() #{result}s"
-        
+        create_forex_time_scale_data(time_range_store)
         time_range_store = Array.new
         p total
     end
@@ -80,19 +70,24 @@ end
 if  0 < time_range_store.count
     #forex_time_scale_data_record_array = db.select_forex_time_scale_data_record_array(time_range_store)
     #time_scale_db.insert_forex_time_scale_data_record_array(forex_time_scale_data_record_array)
-    forex_time_scale_data_record_array = Array.new
-    result = Benchmark.realtime do
-        forex_time_scale_data_record_array = db.select_forex_time_scale_data_record_array(time_range_store)
-    end
-    p "db.select_forex_time_scale_data_record_array() #{result}s"
-        
-    result = Benchmark.realtime do
-        time_scale_db.insert_forex_time_scale_data_record_array(forex_time_scale_data_record_array)
-    end
-    p "time_scale_db.insert_forex_time_scale_data_record_array() #{result}s"
+    create_forex_time_scale_data(time_range_store)
 end
 
 
 
 
 p currency_pair + " " + time_scale_s + " " + start_s + " " + end_s
+
+
+def create_forex_time_scale_data(time_range_store)
+    forex_time_scale_data_record_array = Array.new
+    result = Benchmark.realtime do
+        forex_time_scale_data_record_array = @db.select_forex_time_scale_data_record_array(time_range_store)
+    end
+    p "db.select_forex_time_scale_data_record_array() #{result}s"
+        
+    result = Benchmark.realtime do
+        @time_scale_db.insert_forex_time_scale_data_record_array(forex_time_scale_data_record_array)
+    end
+    p "time_scale_db.insert_forex_time_scale_data_record_array() #{result}s"
+end
